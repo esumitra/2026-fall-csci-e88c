31413b1c71aec5f785f536a7907be42bf1b14cee
esumitra <edward.sumitra@gmail.com>
2026-09-05 17:33:37 -0400

    feat: add week 2 assignment

---
+++ core/src/test/scala/org/cscie88c/core/week2/UtilFunctionsTest.scala
@@ -0,0 +1,17 @@
+package org.cscie88c.core.week2
+
+import org.cscie88c.core.testutils.{StandardTest}
+
+class UtilFunctionsTest extends StandardTest {
+  
+  "UtilFunctions" when {
+    "isPalindrome" should {
+      "return true for a palindrome word" in {
+        UtilFunctions.isPalindrome("tenet") should be (true)
+      }
+    }
+
+    // add unit tests for median below
+    
+  }
+}
